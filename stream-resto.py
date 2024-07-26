c0299a41db91406ccbd88a1532f7e59d8923a8f8
unknown <22611171@students.uii.ac.id>
2024-07-26 10:30:17 +0700

    all file updated

---
+++ stream-resto.py
@@ -0,0 +1,43 @@
+from pyexpat import model
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import mean_squared_error
+
+# Judul aplikasi
+st.title('Aplikasi Prediksi Profit')
+# Membaca data dari file CSV
+df = pd.read_csv('restaurant_menu_optimization_data.csv')
+
+# Menampilkan dataframe
+st.write('Dataframe:')
+st.dataframe(df)
+
+# Memilih fitur dan target
+features = ['MenuCategory', 'Price']
+
+X = df[features]
+y = df['Profitability Number']
+
+# Membagi data menjadi training dan testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Inisialisasi model Decision Tree
+decision_tree = DecisionTreeClassifier()
+# Latih model Decision Tree pada data pelatihan
+decision_tree.fit(X_train, y_train)
+# Lakukan prediksi pada data pengujian
+y_pred = decision_tree.predict(X_test)
+
+
+# Membuat input untuk prediksi user
+st.write('Masukkan nilai untuk prediksi:')
+feature1 = st.number_input('Menu Category')
+feature2 = st.number_input('Price')
+
+# Membuat prediksi berdasarkan input user
+if st.button('Prediksi'):
+    user_input = [[feature1, feature2,]]
+    prediksi_profit = model.predict(user_input)
+    st.write(f'Prediksi Profit: {prediksi_profit[0]}')
